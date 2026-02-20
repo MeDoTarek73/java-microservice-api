e4d5ad92edac61cc593afabc760e2e891b3f3cfb
MeDo TaRek <Mohamed_taree2001@yahoo.com>
2026-02-20 12:09:41 +0200

    Change greeting message in GreetingResource

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from REST API - From feature/activation-fee";
+        return "Hello from REST API - From pre-test";
     }
 }
